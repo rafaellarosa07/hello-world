a73a9941e6cac31a5a8e68a77e3a8a9e3a298119
Rafaella <rafaellarosa07@gmail.com>
2020-06-22 19:09:15 +0000

    ajustes

--- scripts/deleta-aplicacao.bat
+++ scripts/deleta-aplicacao.bat
@@ -1,2 +1,1 @@
-del /f C:\Users\Administrator\Desktop\jboss-eap-6.4.0\jboss-eap-6.4\standalone\deployments\*
-Y
+del /f C:\Users\Administrator\Desktop\jboss-eap-6.4.0\jboss-eap-6.4\standalone\deployments\* /Y
